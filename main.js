23d40dd641c2484c1cd96f0c12694fefbc66b686
H <68972646+hnfdm@users.noreply.github.com>
2025-01-28 20:06:30 +0700

    Update main.js

--- main.js
+++ main.js
@@ -87,8 +87,8 @@ class WalletDashboard {
       "Accept-Encoding": "gzip, deflate, br",
       "Accept-Language": "en-US,en;q=0.9",
       "Content-Type": "application/json",
-      Origin: "https://referralapi.layeredge.io",
-      Referer: "https://referralapi.layeredge.io/",
+      Origin: "https://dashboard.layeredge.io",
+      Referer: "https://dashboard.layeredge.io/",
       "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
     },
