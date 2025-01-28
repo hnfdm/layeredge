736ad2c03d35f9a43c955dc6cd0a948abf9fc2dc
ZAMZA <salimzamza@gmail.com>
2025-01-28 20:15:20 +0700

    Update main.js
    increase timeout

--- main.js
+++ main.js
@@ -92,7 +92,7 @@ class WalletDashboard {
       "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
     },
-    timeout: 30000,
+    timeout: 60000,
   };
 
   // Jika proxy digunakan, tambahkan konfigurasi proxy
